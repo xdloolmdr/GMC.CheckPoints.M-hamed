8695dc95c55624d1b37f190b26cbc8309a21535e
xdloolmdr <hamoudat1990@gmail.com>
2025-03-02 11:39:11 +0100

    screens

--- react-checkoint2/my-product-app/src/App.js
+++ react-checkoint2/my-product-app/src/App.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import PlayersList from "./components/PlayersList";
 
 const App = () => {
+  useEffect(() => {
+    const y = 5;
+    const z = y - 5;
+
+    return () => {};
+  }, []);
+
   return (
     <div>
       <h1 className="text-center my-4">FIFA Player Cards</h1>
@@ -12,4 +19,3 @@ const App = () => {
 };
 
 export default App;
-
